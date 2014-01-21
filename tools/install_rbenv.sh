ea9585e2268ca7fd3deffc590a4173c0a2b8c5ac
Matthew Loberg <loberg.matt@gmail.com>
2014-01-21 14:00:22 -0500

    Added script to install rbenv.

---
+++ tools/install_rbenv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Install rbenv
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+cat <<EOF >>~/.bashrc
+export PATH="\$HOME/.rbenv/bin:\$PATH"
+eval "\$(rbenv init -)"
+EOF
+
+# Install ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Restart the shell
+exec $SHELL
