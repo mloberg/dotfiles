3a88059e5485e42ec104f919b0059e0248dcb999
Matthew Loberg <loberg.matt@gmail.com>
2014-01-23 20:25:20 -0500

    Making more cross-system compatible.
    Attempting to make more cross-system compatible by adding system
    specific folders (linux, darwin) and updating the installer.
    
    New installer written in Python, compatible with 2.6 and 2.7. Need to
    test 3 yet. I found that Python was more widely available on fresh
    systems than ruby was. Plus Python is fucking awesome.
    
    Remove bashfu, as I have converted over to bash-it (well, a custom fork
    https://github.com/mloberg/bash-it).
    
    Still in development. Not all features are available or have been
    tested.

---
+++ lib/dotfiles.py
@@ -0,0 +1,71 @@
+import os
+import re
+import imp
+from .utils import find_files, query_yes_no
+from .template import Template
+
+
+class DotfilesBase(object):
+
+    def __init__(self, ext, ignore=[], target='~'):
+        self.ext = ext
+        self.ignore = ignore
+        self.target = os.path.expanduser(target)
+        self.files = []
+
+    def find(self, path):
+        self.files.extend(find_files(path, '*.%s' % self.ext, self.ignore))
+
+    def dumps(self):
+        print(self.files)
+
+    def _get_filename(self, filename):
+        basename = os.path.basename(filename)
+        return '.' + re.sub(re.escape('.' + self.ext) + '$', '', basename)
+
+    def _get_target(self, filename):
+        return os.path.join(self.target, filename)
+
+
+class Dotfiles(DotfilesBase):
+
+    def __init__(self, ext='symlink', ignore=[], target='~'):
+        super(Dotfiles, self).__init__(ext, ignore, target)
+
+    def symlink(self):
+        for dotfile in self.files:
+            dotfile = os.path.abspath(dotfile)
+            self._symlink_file(dotfile)
+
+    def _symlink_file(self, dotfile):
+        filename = self._get_filename(dotfile)
+        target = self._get_target(filename)
+        # TODO: Broken symlinks aren't caught and can't be symlinked
+        if os.path.exists(target):
+            if os.path.realpath(target) == dotfile:
+                return
+            elif query_yes_no("%s exists. Replace?" % target):
+                os.rename(target, target + '.orig')
+            else:
+                return
+        os.symlink(dotfile, target)
+
+
+class DotfileTemplates(DotfilesBase):
+
+    def __init__(self, ext='template.py', ignore=[], target='~'):
+        super(DotfileTemplates, self).__init__(ext, ignore, target)
+
+    def save(self):
+        for template_file in self.files:
+            self._generate_template(template_file)
+
+    def _generate_template(self, template_file):
+        filename = self._get_filename(template_file)
+        target = self._get_target(filename)
+        if (os.path.exists(target) and
+                query_yes_no("%s exists. Replace?" % target) is False):
+            return
+        template = imp.load_source('template', template_file)
+        tmpl = Template(template.template, template.variables)
+        tmpl.render_to_file(target)
---
+++ lib/utils.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import fnmatch
+
+
+def query_yes_no(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif prompt == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please response with 'yes' or 'no'\n")
+
+
+def which(name, flags=os.X_OK):
+    result = []
+    exts = filter(None, os.environ.get('PATHEXT', '').split(os.pathsep))
+    path = os.environ.get('PATH', None)
+    if path is None:
+        return []
+    for p in os.environ.get('PATH', '').split(os.pathsep):
+        p = os.path.join(p, name)
+        if os.access(p, flags):
+            result.append(p)
+        for e in exts:
+            pext = p + e
+            if os.access(pext, flags):
+                result.append(pext)
+    return result
+
+
+def find_files(path, filter_pattern, ignore=[]):
+    results = []
+    for root, dirnames, filenames in os.walk(path):
+        for dirname in dirnames:
+            if dirname in ignore:
+                dirnames.remove(dirname)
+        for dirname in fnmatch.filter(dirnames, filter_pattern):
+            results.append(os.path.join(root, dirname))
+            dirnames.remove(dirname)
+        for filename in fnmatch.filter(filenames, filter_pattern):
+            results.append(os.path.join(root, filename))
+    return results
