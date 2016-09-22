f0a50819bfaf6cc7bdadba9c26032965c7fdd11e
Matthew Loberg <mloberg@nerdery.com>
2016-09-22 15:06:58 -0500

    Refactor install
    Update bash it install to not be a submodule, but clone from the repo

--- bash/install.sh
+++ bash/install.sh
@@ -1,68 +1,63 @@
 #!/usr/bin/env bash
-BASH_IT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bash_it.symlink"
+#
+# Install bash-it (https://github.com/Bash-it/bash-it)
 
-function load_all() {
-    file_type=$1
-    [ ! -d "$BASH_IT/$file_type/enabled" ] && mkdir "$BASH_IT/$file_type/enabled"
-    for src in $BASH_IT/$file_type/available/*; do
-        filename="$(basename ${src})"
-        [ ${filename:0:1} = "_" ] && continue
-        dest="${BASH_IT}/${file_type}/enabled/${filename}"
-        if [ ! -e "${dest}" ]; then
-            ln -s "../available/{$filename}" "${dest}"
-        else
-            echo "File ${dest} exists, skipping"
-        fi
-    done
+# Install path
+BASH_IT="$HOME/.bash_it"
+
+# Clone the repo
+if [ ! -d "$BASH_IT" ]; then
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git "$BASH_IT"
+fi
+
+## START: Functions from the bash-it install script ##
+
+# enable a thing
+function load_one() {
+  file_type=$1
+  file_to_enable=$2
+  mkdir -p "$BASH_IT/${file_type}/enabled"
+
+  dest="${BASH_IT}/${file_type}/enabled/${file_to_enable}"
+  if [ ! -e "${dest}" ]; then
+    ln -sf "../available/${file_to_enable}" "${dest}"
+  else
+    echo "File ${dest} exists, skipping"
+  fi
 }
 
+# Interactively enable several things
 function load_some() {
-    file_type=$1
-    for path in `ls $BASH_IT/$file_type/available/[^_]*`; do
-        if [ ! -d "$BASH_IT/$file_type/enabled" ]; then
-            mkdir "$BASH_IT/$file_type/enabled"
-        fi
-        file_name=$(basename "$path")
-        while true; do
-            read -p "Would you like to enable the ${file_name%%.*} $file_type? [Y/N] " RESP
-            case $RESP in
-                [yY])
-                    ln -s "../available/$file_name" "$BASH_IT/$file_type/enabled"
-                    break
-                    ;;
-                [nN])
-                    break
-                    ;;
-                *)
-                    echo "Please choose y or n."
-                    ;;
-            esac
-        done
+  file_type=$1
+  [ -d "$BASH_IT/$file_type/enabled" ] || mkdir "$BASH_IT/$file_type/enabled"
+  for path in `ls $BASH_IT/${file_type}/available/[^_]*`
+  do
+    file_name=$(basename "$path")
+    while true
+    do
+      read -e -n 1 -p "Would you like to enable the ${file_name%%.*} $file_type? [y/N] " RESP
+      case $RESP in
+      [yY])
+        ln -s "../available/${file_name}" "$BASH_IT/$file_type/enabled"
+        break
+        ;;
+      [nN]|"")
+        break
+        ;;
+      *)
+        echo -e "\033[91mPlease choose y or n.\033[m"
+        ;;
+      esac
     done
+  done
 }
+
+## END: bash-it install functions ##
 
 for type in "aliases" "plugins" "completion"; do
-    # Run only if not ran before
-    if [ ! "$(ls -A $BASH_IT/$type/enabled)" ] || [ "$1" = "--update" ]; then
-        while true; do
-            read -p "Would you like to enable all, some, or no $type? Some of these may make bash slower to start up (especially completion). (all/some/none) " RESP
-            case $RESP in
-                some)
-                    load_some $type
-                    break
-                    ;;
-                all)
-                    load_all $type
-                    break
-                    ;;
-                none)
-                    break
-                    ;;
-                *)
-                    echo "Unknown choice. Please enter some, all, or none"
-                    continue
-                    ;;
-            esac
-        done
-    fi
+  # Run only if not ran before
+  if [ ! "$(ls -A $BASH_IT/$type/enabled 2>/dev/null)" ] || [ "$1" = "--update" ]; then
+    echo -e "\033[0;32mEnabling $type\033[0m"
+    load_some $type
+  fi
 done
--- system/_path.bash
+++ system/_path.bash
@@ -1,1 +1,1 @@
-export PATH="./bin:./node_modules/.bin:$HOME/bin:$DOTFILES/bin:$PATH"
+export PATH="./bin:./node_modules/.bin:$HOME/bin:$DOTFILES/bin:/usr/local/bin:$PATH"
