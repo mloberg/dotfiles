68466279aa15f866d0f419aa255f694840d1ea98
Matthew Loberg <loberg.matt@gmail.com>
2016-09-27 17:02:13 -0500

    Fix Hot Corners in macOS setup

--- macos/setup.sh
+++ macos/setup.sh
@@ -50,9 +50,14 @@ if [[ "$@" != *"--force"* ]]; then
   fi
 fi
 
-# Here we go.. ask for the administrator password upfront and run a
-# keep-alive to update existing `sudo` time stamp until script has finished
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
 sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
@@ -128,7 +133,7 @@ defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 # info "Disable the 'Are you sure you want to open this application?' dialog"
 # defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-info "Remove duplicates in the 'Open With' menu (also see \`lscleanup\` alias)"
+info "Remove duplicates in the 'Open With' menu"
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
 info "Display ASCII control characters using caret notation in standard text views"
@@ -461,10 +466,10 @@ info "Hot corner: Top right screen corner → Mission Control"
 defaults write com.apple.dock wvous-tr-corner -int 2
 defaults write com.apple.dock wvous-tr-modifier -int 0
 info "Hot corner: Bottom left screen corner → no-op"
-defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-bl-modifier -int 0
 info "Hot corner: Bottom right screen corner → Desktop"
-defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
 
 ###############################################################################
