3a82c51d57916a179d3656fee002270c8a44d888
Matthew Loberg <loberg.matt@gmail.com>
2017-03-18 09:20:18 -0500

    Fix Ruby install

--- ruby/install.sh
+++ ruby/install.sh
@@ -10,10 +10,6 @@ default_gems_file=$(rbenv root)/default-gems
 
 (cat "${default_gems_file}" | grep bundler) &>/dev/null | (echo 'bundler' >> "${default_gems_file}")
 
-# rbenv install 1.9.3-p545
-# rbenv install 2.0.0-p648
-# rbenv install 2.1.10
-# rbenv install 2.2.5
-rbenv install 2.3.1
+(rbenv versions | grep 2.3.1) &>/dev/null || rbenv install 2.3.1
 
 rbenv global 2.3.1
