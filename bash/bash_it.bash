e6ec5886c1993b092be0a0aea3de82ea0fa116d3
Matthew Loberg <loberg.matt@gmail.com>
2017-03-18 10:46:28 -0500

    Add Bash It install

---
+++ bash/bash_it.bash
@@ -0,0 +1,8 @@
+# Path to bash it root
+export BASH_IT="$HOME/.bash_it"
+
+# Don't check mail when opening terminal.
+unset MAILCHECK
+
+# Load Bash It
+source $BASH_IT/bash_it.sh
--- bash/install.sh
+++ bash/install.sh
@@ -9,3 +9,17 @@ if [ -f "/usr/local/bin/bash" ]; then
         chsh -s /usr/local/bin/bash
     fi
 fi
+
+if [ -z "$BASH_IT" ]; then
+    export BASH_IT="$HOME/.bash_it"
+fi
+
+if [ ! -d "$BASH_IT" ]; then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git "$BASH_IT"
+    "$BASH_IT/install.sh" --silent --no-modify-config
+    source $BASH_IT/bash_it.sh
+fi
+
+bash-it enable alias bundler osx vagrant
+bash-it enable completion brew bundler capistrano composer docker-compose docker gem git gulp makefile npm ssh tmux vagrant
+bash-it enable plugin extract tmux
