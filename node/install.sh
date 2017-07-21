87d47af6635ac68d919694c9b5e7fc456402a551
Matthew Loberg <loberg.matt@gmail.com>
2017-07-21 09:39:22 -0500

    Fix Node install

--- node/install.sh
+++ node/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
 # Install some Node versions with n
+if [ ! -d /usr/local/n ]; then
+    sudo mkdir -p /usr/local/n
+    sudo chown $(whoami):$(groups | cut -d" " -f1) /usr/local/n
+fi
 
-# Make sure it's installed
-n stable
-
-# Make sure it's the active version
-n stable
+# Install and activate the LTS version
+n lts && n lts
