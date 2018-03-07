f19f9d28027a0b47950752b173e73586233c9acc
Matt Loberg <loberg.matt@gmail.com>
2018-03-06 23:20:00 -0600

    Cleanup duplicate aliases from bash-it

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,25 +1,5 @@
-alias fuck='sudo $(history -p \!\!)'
 alias start_xdebug='export XDEBUG_CONFIG="idekey=PHPSTORM"'
 alias stop_xdebug='unset XDEBUG_CONFIG'
-
-alias reload!='. ~/.bash_profile'
-
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-  alias ls='ls --color=auto'
-  #alias dir='dir --color=auto'
-  #alias vdir='vdir --color=auto'
-
-  alias grep='grep --color=auto'
-  alias fgrep='fgrep --color=auto'
-  alias egrep='egrep --color=auto'
-fi
-
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
