a8ad0deb046137fbb805c5cd0ff76e38176b9845
Matt Loberg <loberg.matt@gmail.com>
2018-03-06 23:13:31 -0600

    Cleanup prompt theme

--- bash/bash_it.bash
+++ bash/bash_it.bash
@@ -1,6 +1,9 @@
 # Path to bash it root
 export BASH_IT="$HOME/.bash_it"
 
+# Bash prompt theme
+export BASH_IT_THEME="$HOME/.dotfiles/bash/prompt.bash"
+
 # Don't check mail when opening terminal.
 unset MAILCHECK
 
