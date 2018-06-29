f24f3239af62044ea1ac0d018c822224dddc499b
Matt Loberg <loberg.matt@gmail.com>
2018-06-28 19:48:44 -0500

    add composer docker function
    Instead of having to have composer installed, run composer via Docker.

--- bash.d/php.bash
+++ bash.d/php.bash
@@ -6,3 +6,17 @@ fi
 
 pathmunge "$COMPOSER_HOME/vendor/bin"
 pathmunge ./vendor/bin
+
+function composer() {
+    tty=
+    tty -s && tty=--tty
+    docker run $tty \
+        --interactive \
+        --rm \
+        --user $(id -u):$(id -g) \
+        --volume /etc/passwd:/etc/passwd:ro \
+        --volume /etc/group:/etc/group:ro \
+        --volume $COMPOSER_HOME/.composer:/tmp \
+        --volume $(pwd):/app \
+        composer "$@"
+}
