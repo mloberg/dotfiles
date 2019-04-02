e7564f3b2896c9fe731a70e64d6494d49749f2c7
Matt Loberg <mloberg@nerdery.com>
2019-04-02 09:23:51 -0500

    improve docker services
    renamed to dockter, hopefully a more unqiue name will help me remember
    it.
    
    Add commands for ls, status, and run.
    
    Improve redis-cli and portainer aliases

--- bash.d/docker.bash
+++ bash.d/docker.bash
@@ -15,30 +15,56 @@ function docker-compose-fresh() {
     COMPOSE_FILE_PARAM="--file $1"
   fi
 
-  docker-compose $COMPOSE_FILE_PARAM down
-  docker-compose $COMPOSE_FILE_PARAM up -d
+  docker-compose "${COMPOSE_FILE_PARAM}" down
+  docker-compose "${COMPOSE_FILE_PARAM}" up -d
 }
 
-function services() {
-  local ACTION=""
+# dockter ls
+# dockter status redis
+# dockter start portainer
+# dockter stop mysql
+# dockter run redis redis-cli
+function dockter() {
+  local ACTION="$1"
   local SERVICE="$2"
 
-  case "$1" in
-    start)
-      ACTION="up -d"
-      ;;
-    stop)
-      ACTION="rm -sfv"
-      ;;
-  esac
+  if [ "$ACTION" == "ls" ]; then
+    docker-compose --file ~/.services.yml ps --services
+    return
+  fi
 
   if [[ -z "$ACTION" || -z "$SERVICE" ]]; then
-    echo "Usage: services [start|stop] [service]"
+    >&2 echo "Usage: dockter [start|stop|status|run] [service]"
     return 1
   fi
 
-  docker-compose --file ~/.services.yml $ACTION $SERVICE
+  case "$ACTION" in
+    status)
+      _dockter-status "${SERVICE}"
+      ;;
+    start)
+      docker-compose --file ~/.services.yml up -d "${SERVICE}"
+      ;;
+    stop)
+      docker-compose --file ~/.services.yml rm -sfv "${SERVICE}"
+      ;;
+    run)
+      docker-compose --file ~/.services.yml exec "${SERVICE}" "${3:-bash}"
+      ;;
+    *)
+      >&2 echo "Usage: dockter [start|stop|status|run] [service]"
+      return 1
+      ;;
+  esac
 }
 
-alias redis-cli="docker-compose --file ~/.services.yml exec redis redis-cli"
-alias portainer="open http://localhost:9000"
+function _dockter-status() {
+  if docker-compose --file ~/.services.yml ps | grep "$1" >/dev/null; then
+    echo "Service ${1} is running"
+  else
+    echo "Service ${1} is stopped"
+  fi
+}
+
+alias redis-cli="dockter run redis redis-cli"
+alias portainer="(dockter status portainer | grep running >/dev/null || dockter start portainer) && open http://localhost:9000"
