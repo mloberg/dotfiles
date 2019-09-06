ccf1dc510d5d456432bdf87677a0d47003ec61d6
Matt Loberg <mloberg@nerdery.com>
2019-09-06 07:33:02 -0500

    update docker compose

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -9,48 +9,51 @@ function docker-clean() {
   docker images --filter dangling=true -q | xargs docker rmi
 }
 
-# service ls|list
-# service status [service]
-# service start [service]
-# service stop [service]
-# service run [service] [command]
-function service() {
+# dcg ls|list
+# dcg status [service]
+# dcg start [service]
+# dcg stop [service]
+# dcg run [service] [command]
+function dcg() {
   local ACTION="$1"
   local SERVICE="$2"
 
   if [[ "$ACTION" == "ls" || "$ACTION" == "list" ]]; then
-    docker-compose --file ~/.services.yml ps --services
+    docker-compose --file ~/.docker-compose.yml ps --services
     return
   fi
 
   if [[ -z "$ACTION" || -z "$SERVICE" ]]; then
-    >&2 echo "Usage: service [start|stop|status|run] [service]"
+    >&2 echo "Usage: dcg [start|stop|status|run] [service]"
     return 1
   fi
 
   case "$ACTION" in
     status)
-      if docker-compose --file ~/.services.yml ps | grep "${SERVICE}" >/dev/null; then
+      if docker-compose --file ~/.docker-compose.yml ps | grep "${SERVICE}" | grep -q Up; then
         echo "Service ${SERVICE} is running"
       else
         echo "Service ${SERVICE} is stopped"
       fi
       ;;
     start)
-      docker-compose --file ~/.services.yml up -d "${SERVICE}"
+      docker-compose --file ~/.docker-compose.yml up -d "${SERVICE}"
       ;;
     stop)
-      docker-compose --file ~/.services.yml rm -sfv "${SERVICE}"
+      docker-compose --file ~/.docker-compose.yml rm -sfv "${SERVICE}"
       ;;
     run)
-      docker-compose --file ~/.services.yml exec "${SERVICE}" "${3:-bash}"
+      docker-compose --file ~/.docker-compose.yml exec "${SERVICE}" "${3:-bash}"
+      ;;
+    purge)
+      docker-compose --file ~/.docker-compose.yml down -v
       ;;
     *)
-      >&2 echo "Usage: dockter [start|stop|status|run] [service]"
+      >&2 echo "Usage: dcg [start|stop|status|run] [service]"
       return 1
       ;;
   esac
 }
 
-alias redis-cli="service run redis redis-cli"
-alias portainer="(service status portainer | grep running >/dev/null || service start portainer) && open http://localhost:9000"
+alias redis-cli="dcg run redis redis-cli"
+alias portainer="(dcg status portainer | grep -q running || dcg start portainer) && open http://localhost:9000"
