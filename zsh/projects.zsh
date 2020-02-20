cbc71cb633087fa2ce9ba164ecabda9e1d949371
Matt Loberg <mloberg@nerdery.com>
2020-02-20 08:09:14 -0600

    add help to p

--- zsh/projects.zsh
+++ zsh/projects.zsh
@@ -4,13 +4,26 @@ function p() {
     cmd="cd"
     project="$1"
 
+    if [ "$*" =~ "-h" ]; then
+        cat <<EOF
+NAME
+    $0 -- shortcuts for navigating between projects
+
+SYNOPSIS
+    $0 add [alias] [directory]
+    $0 open [alias]
+    $0 [alias]
+EOF
+        return
+    fi
+
     if [ "$project" == "add" ]; then
         project_alias="$2"
         project_path="${3:-$PWD}"
 
         if [ -z "$project_alias" ]; then
             (>&2 echo "Missing alias")
-            exit 1
+            return 1
         fi
 
         echo "$project_alias:$project_path" >> "$HOME/.projects"
