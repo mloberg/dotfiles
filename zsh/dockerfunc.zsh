ae93a85d14f02f81fbfd5e9c69b8a3f1dd7b569a
Matt Loberg <mloberg@nerdery.com>
2020-03-10 08:06:24 -0500

    cleanup dockerfuncs

--- zsh/dockerfunc.zsh
+++ zsh/dockerfunc.zsh
@@ -1,22 +1,21 @@
 # shell wrappers for docker run commands
 # Inspiration taken from: https://github.com/jessfraz/dotfiles/blob/master/.dockerfunc
 
-function aws() {
-  docker run -it --rm \
-    -v "${HOME}/.aws:/root/.aws" \
-    -v "$(pwd):/tmp/data" \
-    -w /tmp/data \
-    --log-driver none \
-    --name aws \
-    jess/awscli --profile "${AWS_PROFILE:-default}" "$@"
+jekyll() {
+  cache=
+  test -f Gemfile && cache="-v $(pwd)/vendor/bundle:/usr/local/bundle"
+  docker run --rm -it \
+    -v "$(pwd):/srv/jekyll" "$cache" \
+    -p 4000:4000 \
+    jekyll/jekyll:"${JEKYLL_VERSION:-4.0}" \
+    jekyll "$@"
 }
 
-function jekyll() {
-  cache=
-  test -f Gemfile && cache="--volume $(pwd)/vendor/bundle:/usr/local/bundle"
+godev() {
+  [ -z "$GO_VERSION" ] && test -f go.mod && GO_VERSION=$(awk '/^go [0-9\.]/{print $2}' go.mod)
   docker run --rm -it \
-    --volume "$(pwd):/srv/jekyll" "$cache" \
-    --publish 4000:4000 \
-    jekyll/jekyll:"${JEKYLL_VERSION:-3.8}" \
-    jekyll "$@"
+    -v "${HOME}/.netrc:/root/.netrc" \
+    -v "$(pwd):/app" \
+    -w /app \
+    mlo/golang:"${GO_VERSION:-1.14}" "$@"
 }
