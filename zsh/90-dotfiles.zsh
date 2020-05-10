44bfcab911b80343daaeab1413c33fa30b25c275
Matt Loberg <loberg.matt@gmail.com>
2020-05-10 12:33:33 -0500

    improve update check

--- zsh/90-dotfiles.zsh
+++ zsh/90-dotfiles.zsh
@@ -1,10 +1,6 @@
-DOTFILES="$HOME/.dotfiles"
-
-function dotfiles() {
-    rcup "$@" && touch "$DOTFILES/.update"
-}
+updatefile="${DOTFILES:=$HOME/.dotfiles}/.update"
 
 # warn if dotfiles haven't been updated in last 7 days
-if [ "$(expr $(date +%s) - $(stat -f "%Sm" -t "%s" "$DOTFILES/.update"))" -gt 604800 ]; then
+if [[ ! -f "$updatefile" || "$(expr $(date +%s) - $(stat -f "%Sm" -t "%s" "$updatefile"))" -gt 604800 ]]; then
     echo "⚠️ dotfiles are out of date run \`dotfiles\` to update⚠️"
 fi
