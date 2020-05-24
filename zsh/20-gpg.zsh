506ef4d1e60b2c703d3aef1189f29a56d46384a2
Matt Loberg <loberg.matt@gmail.com>
2020-05-23 22:21:22 -0500

    feat(gpg): add conceal/reveal functions

--- zsh/20-gpg.zsh
+++ zsh/20-gpg.zsh
@@ -1,3 +1,13 @@
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
+
+alias gpg=gpg2
+
+conceal() {
+    gpg --encrypt --armor -r 0x2355A228A9F1D6CF --output "$1.enc" "$1"
+}
+
+reveal() {
+    gpg --decrypt --output "${1%.enc}" "$1"
+}
