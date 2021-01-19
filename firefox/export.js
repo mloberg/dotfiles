947baf62b0734b9e28795fdafb985253120abc23
Matt Loberg <mloberg@users.noreply.github.com>
2021-01-19 09:49:04 -0600

    make firefox a little more usable

--- firefox/export.js
+++ firefox/export.js
@@ -1,28 +1,17 @@
-var result = [];
-var prefs = Services.prefs.getChildList('');
+// List custom user preferences
+const userPrefs = Services.prefs.getChildList('')
+    .filter((p) => Services.prefs.prefHasUserValue(p))
+    .map((p) => {
+        switch (Services.prefs.getPrefType(p)) {
+            case Services.prefs.PREF_STRING:
+                return `user_pref("${p}", "${Services.prefs.getStringPref(p).replace(/"/g, '\\"')}");`;
+            case Services.prefs.PREF_INT:
+                return `user_pref("${p}", ${Services.prefs.getIntPref(p).toString()});`;
+            case Services.prefs.PREF_BOOL:
+                return `user_pref("${p}", ${Services.prefs.getBoolPref(p).toString()});`;
+            default:
+                return result.push(`//user_pref("${p}", ???);`);
+        }
+    });
 
-for (const i in prefs) {
-    var pref = prefs[i];
-    if (!Services.prefs.prefHasUserValue(pref)) {
-        continue;
-    }
-    var p = pref.replace(/"/g, '\\"');
-	switch (Services.prefs.getPrefType(pref)) {
-		case 32:
-			var v = Services.prefs.getStringPref(pref).replace(/"/g, '\\"');
-			result.push(`user_pref("${p}", "${v}");`);
-			break;
-		case 64:
-			var v = Services.prefs.getIntPref(pref).toString();
-			result.push(`user_pref("${p}", ${v});`);
-			break;
-		case 128:
-			var v = Services.prefs.getBoolPref(pref).toString();
-			result.push(`user_pref("${p}", ${v});`);
-			break;
-		default:
-			result.push(`//user_pref("${p}", ???);`);
-	}
-}
-
-console.log(result.join('\n'));
+console.log(userPrefs.join('\n'));
---
+++ firefox/user.js
@@ -0,0 +1,27 @@
+// disable about:config warning
+user_pref("browser.aboutConfig.showWarning", false);
+
+// don't check if Firefox is the default browser
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+// disable sending the URL of the website where a plugin crashed
+user_pref("dom.ipc.plugins.reportCrashURL", false);
+
+// disable about:addons' Recommendations pane
+user_pref("extensions.getAddons.showPane", false);
+// disable recommendations in about:addons' Extensions and Themes panes
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+
+// disable FTP
+user_pref("network.ftp.enabled", false);
+
+// limit the number of closed tabs Firefox remembers
+user_pref("browser.sessionstore.max_tabs_undo", 5);
+
+// send only the scheme, host, and port in the Referer header
+user_pref("network.http.referer.trimmingPolicy", 2);
+// limit referer to only send the scheme, host, and port
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+
+// increase on some confirmation dialogs to 3s
+user_pref("security.dialog_enable_delay", 3000);
