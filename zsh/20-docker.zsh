9c99b8bccec5b8d09e8bf8f6e2cbd2fbc9a3df7b
Matt Loberg <loberg.matt@gmail.com>
2021-02-28 12:10:50 -0600

    add global docker-compose for common services
    Create a "service" command that manages common services like databases
    via a global Docker Compose file

--- zsh/20-docker.zsh
+++ zsh/20-docker.zsh
@@ -23,38 +23,63 @@ del_stopped() {
   [ "$state" == "false" ] && docker rm "$name"
 }
 
+# Work with local services via Docker Compose. `service help` for help
+service() {
+  case "$1" in
+    ls|list)
+      service ps --services
+      ;;
+    start)
+      service up -d "${@:2}"
+      ;;
+    stop)
+      service rm -sfv "${@:2}"
+      ;;
+    status)
+      if [ "$(service ps --filter status=up -q "$2")" == "" ]; then
+        echo "$2 is stopped"
+      else
+        echo "$2 is running"
+      fi
+      ;;
+    run)
+      service exec "${@:2}"
+      ;;
+    port)
+      # make sure the service is running
+      [ "$(service ps --filter status=up -q "$2")" == "" ] && echo "$2 is not running" && return 1
+
+      port="$3"
+      if [ -z "$port" ]; then
+        # install yaml2json if not already
+        command -v yaml2json &>/dev/null || go get -v github.com/bronze1man/yaml2json
+
+        # grab the first listed port
+        port=$(service config | yaml2json | jq -r ".services.$2.ports[0].target")
+      fi
+
+      service -- port "$2" "$port"
+      ;;
+    -h|--help|help|"")
+      cat <<EOF
+Wrapper around Docker Compose to work with local services like Redis, Postgres, and MySQL
+
+    service list                        List available services
+    service start <service>             Start a Service
+    service stop <service>              Stop a service
+    service status <service>            Is service running?
+    service run <service> <command>     Run a command in the service container
+    service port <service> [port]       Display the local port binding for the service
+EOF
+      ;;
+    *)
+      docker-compose -f "$HOME/.services.yml" "$@"
+      ;;
+  esac
+}
+
 # shell wrappers for docker run commands
 # Inspiration taken from: https://github.com/jessfraz/dotfiles/blob/master/.dockerfunc
-
-redis() {
-  del_stopped redis
-  docker run -d \
-    -p 6379:6379 \
-    --name redis \
-    redis:alpine redis-server --appendonly yes
-}
-alias redis-cli="docker exec -it redis redis-cli"
-
-portainer() {
-  del_stopped portainer
-  docker run -d \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -p 9000:9000 \
-    --name portainer \
-    portainer/portainer -H unix:///var/run/docker.sock
-  echo
-  echo "Portainer started on http://localhost:9000"
-}
-
-jekyll() {
-  cache=
-  test -f Gemfile && cache="-v $(pwd)/vendor/bundle:/usr/local/bundle"
-  docker run --rm -it \
-    -v "$(pwd):/srv/jekyll" "$cache" \
-    -p 4000:4000 \
-    jekyll/jekyll:"${JEKYLL_VERSION:-4.0}" \
-    jekyll "$@"
-}
 
 godev() {
   [ -z "$GO_VERSION" ] && test -f go.mod && GO_VERSION=$(awk '/^go [0-9\.]/{print $2}' go.mod)
