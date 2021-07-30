aac1802aa40700af15ead1aa61aa23578c36d102
Matt Loberg <mloberg@articulate.com>
2021-07-30 07:23:39 -0500

    add repo func to checkout projects

--- zsh/20-functions.zsh
+++ zsh/20-functions.zsh
@@ -2,3 +2,19 @@
 function mk {
   mkdir -p "$1" && cd $_
 }
+
+repo() {
+  local repo="$1"
+  dir="${SRC:-$HOME/src}/$(basename "$repo")"
+  shift
+
+  if [ -z "$repo" ]; then
+    echo "usage: $0 <repo>" >&2
+    exit 1
+  fi
+
+  [[ "$repo" != */* ]] && repo="${GITHUB_ORG:-articulate}/$repo"
+
+  [ -d "$dir" ] || gh repo clone "$repo" "$dir" -- "$@"
+  cd "$dir" || exit 1
+}
