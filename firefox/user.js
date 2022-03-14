ee4f4bc86376b75f2e0ed895a49b366283936e56
Matt Loberg <mloberg@articulate.com>
2022-03-14 09:12:04 -0500

    feat(firefox): more privacy settings

--- firefox/user.js
+++ firefox/user.js
@@ -12,6 +12,9 @@ user_pref("extensions.getAddons.showPane", false);
 // disable recommendations in about:addons' Extensions and Themes panes
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 
+// disable pocket
+user_pref("extensions.pocket.enabled", false);
+
 // disable FTP
 user_pref("network.ftp.enabled", false);
 
@@ -26,7 +29,7 @@ user_pref("network.http.referer.trimmingPolicy", 2);
 // limit referer to only send the scheme, host, and port
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 
-// increase on some confirmation dialogs to 3s
+// increase some confirmation dialogs to 3s
 user_pref("security.dialog_enable_delay", 3000);
 
 // disabled sponsored content
@@ -36,8 +39,40 @@ user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 // open new tab to the right of the active tab
 user_pref("browser.tabs.insertAfterCurrent", true);
 
-// additional privacy settings
+// prevent websites from checking the state of microphone and camera
+user_pref("media.navigator.enabled", false);
+
+// isolate first party domain cookies to reduce tracking across websites
 user_pref("privacy.firstparty.isolate", true);
+
+// prevent crypto miners
+user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+
+// attempt to reduce the browser's fingerprint
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+
+// disable location services (as they go through Google's servers)
+user_pref("geo.enabled", false);
+
+// disable pre-fetching webpages
+user_pref("network.prefetch-next", false);
+
+// disable websockets as it can leak your real ip on a VPN
+user_pref("network.websocket.max-connections", 0);
+
+// prevent clipboard notifications
+user_pref("dom.event.clipboardevents.enabled", false);
+
+// disable webRTC as it can leak your real ip on a VPN
+user_pref("media.peerconnection.enabled", false);
+
+// prevent websites from ready battery level
+user_pref("dom.battery.enabled", false);
+
+// prevent websites from tracking clicks on a page
+user_pref("browser.send_pings", false);
+
+// additional privacy settings
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("webgl.disabled", true);
 user_pref("beacon.enabled", false);
