00b23a43f8d035d321f8835feb809a6121661938
Matt Loberg <mloberg@users.noreply.github.com>
2022-07-26 13:35:20 -0500

    feat(go): add alias to show outdated modules

--- zsh/20-go.zsh
+++ zsh/20-go.zsh
@@ -1,2 +1,4 @@
 export GOPATH=${GOPATH:-"$HOME/go"}
 export PATH="${GOPATH}/bin:$PATH"
+
+alias go-mod-outdated="go list -m -u -json all | jq -rs '.[] | select(.Indirect | not) | select(.Main | not) | select(.Update) | .Path + \" \" + .Version + \" => \" + .Update.Version'"
