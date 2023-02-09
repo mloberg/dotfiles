b3ea54dc6cc5d9b1306785a7d4527d13d7fd12d5
Matt Loberg <mloberg@users.noreply.github.com>
2023-02-08 20:24:48 -0600

    switch to antidote

--- zsh/20-go.zsh
+++ zsh/20-go.zsh
@@ -2,3 +2,5 @@ export GOPATH=${GOPATH:-"$HOME/go"}
 export PATH="${GOPATH}/bin:$PATH"
 
 alias go-mod-outdated="go list -m -u -json all | jq -rs '.[] | select(.Indirect | not) | select(.Main | not) | select(.Update) | .Path + \" \" + .Version + \" => \" + .Update.Version'"
+alias go-histogram="go list -json ./... | jq -rc '[.ImportPath, (.GoFiles | length | tostring)] | join(\" \")' | perl -lane 'print (\" \" x (20 - \$F[1]), \"=\" x \$F[1], \" \", \$F[1], \"\t\", \$F[0])'"
+alias go-notests="go list -json ./... | jq -rc 'select((.TestGoFiles | length)==0) | .ImportPath'"
