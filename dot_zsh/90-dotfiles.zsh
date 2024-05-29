14fb0af020a9166cd3105f6eab94401e3801e78c
Matt Loberg <mloberg@users.noreply.github.com>
2024-05-29 16:20:23 -0500

    update instructions for chezmoi

--- dot_zsh/90-dotfiles.zsh
+++ dot_zsh/90-dotfiles.zsh
@@ -1,4 +1,4 @@
 # warn if dotfiles haven't been updated in last 7 days
-if [[ -n ${DOTFILES:=$HOME/.dotfiles}/.update(#qN.m+7) ]]; then
-    echo "⚠️ dotfiles are out of date run \`dotfiles\` to update⚠️"
+if [[ -n ${HOME}/.update(#qN.m+7) ]]; then
+    echo "⚠️ dotfiles are out of date run \`chezmoi update\` to update⚠️"
 fi
